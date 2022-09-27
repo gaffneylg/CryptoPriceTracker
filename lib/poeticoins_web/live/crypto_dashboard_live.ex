ca294e8cb382c0a23c36e39f75c2f387d787358c
gaffneylg <gaffneylg@gmail.com>
2022-09-27 21:11:52 +0100

    breakout of LV product_component from dashboard

--- lib/poeticoins_web/live/crypto_dashboard_live.ex
+++ lib/poeticoins_web/live/crypto_dashboard_live.ex
@@ -4,83 +4,52 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
   import PoeticoinsWeb.ProductHelpers
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, trades: %{}, products: [])
+    socket = assign(socket, products: [])
     {:ok, socket}
   end
 
-  # def render(assigns) do
-  #   ~L"""
-  #   <div class="poeticoins-toolbar">
-  #     <div class="title">Crypto Tracker</div>
+  def render(assigns) do
+    ~H"""
+    <div class="poeticoins-toolbar">
+      <div class="title">Crypto Tracker</div>
 
-  #     <form action="#" phx-submit="add-product">
-  #       <select name="product_id" class="select-product">
+      <form action="#" phx-submit="add-product">
+        <select name="product_id" class="select-product">
 
-  #         <option selected disabled>Add a Crypto Product</option>
+          <option selected disabled>Add a Crypto Product</option>
 
-  #         <%= for {exchange_name, products} <- group_products_by_exchange() do %>
-  #           <optgroup label="<%= exchange_name %>">
-  #             <%= for product <- products do %>
-  #               <option value="<%= to_string(product) %>">
-  #                 <%= crypto_name(product) %>
-  #                 -
-  #                 <%= currency_char(product) %>
-  #               </option>
-  #             <% end %>
-  #           </optgroup>
-  #         <% end %>
-  #       </select>
+          <%= for {exchange_name, products} <- group_products_by_exchange() do %>
+            <optgroup label={"#{exchange_name}"}>
+              <%= for product <- products do %>
+                <option value={ "#{to_string(product)}" }>
+                  <%= crypto_name(product) %>
+                  -
+                  <%= currency_char(product) %>
+                </option>
+              <% end %>
+            </optgroup>
+          <% end %>
+        </select>
 
-  #       <button type="submit" phx-disable-with="Loading...">+</button>
-  #     </form>
-  #   </div>
+        <button type="submit">+</button>
+      </form>
+    </div>
 
-  #   <div class="product-components">
-  #     <%= for product <- @products, trade = @trades[product], not is_nil(trade) do %>
-  #       <div class="product-component">
-  #         <div class="currency-container">
-  #           <img class="icon" src="<%= crypto_icon(@socket, product) %>" />
-  #           <div class="crypto-name">
-  #             <%= crypto_name(product) %>
-  #           </div>
-  #         </div>
-
-  #         <div class="price-container">
-  #           <ul class="currency-symbols">
-  #             <%= for curr <- currency_icons() do %>
-  #               <li class="<%= if currency_symbol(product) == curr, do: "active" %>">
-  #                 <%= curr %>
-  #               </li>
-  #             <% end %>
-  #           </ul>
-
-  #           <div class="price">
-  #             <%= currency_char(product) %>
-  #             <%= trade.price %>
-  #           </div>
-  #         </div>
-
-  #         <div class="exchange-name">
-  #           <%= product.exchange_name %>
-  #         </div>
-
-  #         <div class="trade-time">
-  #           <%= human_datetime(trade.traded_at) %>
-  #         </div>
-  #       </div>
-  #     <% end %>
-  #   </div>
-  #   """
-  # end
+    <div class="product-components">
+      <%= for product <- @products do %>
+        <%= live_component(PoeticoinsWeb.ProductComponent, id: product) %>
+      <% end %>
+    </div>
+    """
+  end
 
   def handle_info({:new_trade, trade}, socket) do
-    socket = update(socket, :trades, &Map.put(&1, trade.product, trade))
+    send_update(PoeticoinsWeb.ProductComponent, id: trade.product, trade: trade)
     {:noreply, socket}
   end
 
   def handle_event("add-product", %{"product_id" => product_id} = _params, socket) do
-    [exchange, currency_pair] = String.split(product_id, ":")
-    product = Product.new(exchange, currency_pair)
+    product = product_from_string(product_id)
     socket = add_product_check(socket, product)
     {:noreply, socket}
   end
@@ -99,15 +68,24 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
       {:noreply, assign(socket, :products, products)}
   end
 
+  def handle_event("remove-product", %{"product-id" => product_id} = _params, socket) do
+    product = product_from_string(product_id)
+    socket = update(socket, :products, &List.delete(&1, product))
+    {:noreply, socket}
+  end
+
   defp add_product_check(socket, product) do
     if product not in socket.assigns.products do
       socket
       |> add_product(product)
-      |> put_flash(:info, "#{product.exchange_name} - #{product.currency_pair} added successfully.")
     else
       socket
-      |> put_flash(:error, "The product was already added.")
     end
+  end
+
+  defp product_from_string(product_id) do
+    [exchange, currency_pair] = String.split(product_id, ":")
+    Product.new(exchange, currency_pair)
   end
 
   def group_products_by_exchange do
@@ -119,9 +97,5 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
     Poeticoins.subscribe_to_trades(product)
     socket
     |> update(:products, & &1 ++ [product])
-    |> update(:trades, fn trades ->
-      trade = Poeticoins.get_last_trade(product)
-      Map.put(trades, product, trade)
-    end)
   end
 end
---
+++ lib/poeticoins_web/live/product_component.ex
@@ -0,0 +1,96 @@
+defmodule PoeticoinsWeb.ProductComponent do
+  use PoeticoinsWeb, :live_component
+
+  import PoeticoinsWeb.ProductHelpers
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def update(%{trade: trade}=_assigns, socket) when not is_nil(trade) do
+    socket = assign(socket, :trade, trade)
+    {:ok, socket}
+  end
+
+  def update(assigns, socket) do
+    product = assigns.id
+    socket =
+      assign(socket,
+        product: product,
+        trade: Poeticoins.get_last_trade(product))
+    {:ok, socket}
+  end
+
+  def render(%{trade: trade} = assigns) when not is_nil(trade) do
+    ~H"""
+      <div class="product-component">
+        <button class="remove" phx-click="remove-product"
+          phx-value-product-id={ "#{to_string(@product)}" }>X</button>
+        <div class="currency-container">
+          <img class="icon" src={"#{crypto_icon(@socket, @product)}"}/>
+          <div class="crypto-name">
+            <%= crypto_name(@product) %>
+          </div>
+        </div>
+
+        <div class="price-container">
+          <ul class="currency-symbols">
+            <%= for curr <- currency_icons() do %>
+              <li class={"#{if currency_symbol(@product) == curr, do: 'active' }"}>
+                <%= curr %>
+              </li>
+            <% end %>
+          </ul>
+
+          <div class="price">
+            <%= currency_char(@product) %>
+            <%= @trade.price %>
+          </div>
+        </div>
+
+        <div class="exchange-name">
+          <%= @product.exchange_name %>
+        </div>
+
+        <div class="trade-time">
+          <%= human_datetime(@trade.traded_at) %>
+        </div>
+      </div>
+    """
+  end
+
+  def render(assigns) do
+    ~H"""
+      <div class="product-component">
+        <div class="currency-container">
+          <img class="icon" src={"#{crypto_icon(@socket, @product)}"}/>
+          <div class="crypto-name">
+            <%= crypto_name(@product) %>
+          </div>
+        </div>
+
+        <div class="price-container">
+          <ul class="currency-symbols">
+            <%= for curr <- currency_icons() do %>
+              <li class={"#{if currency_symbol(@product) == curr, do: 'active' }"}>
+                <%= curr %>
+              </li>
+            <% end %>
+          </ul>
+
+          <div class="price">
+            <%= currency_char(@product) %>
+            ...
+          </div>
+        </div>
+
+        <div class="exchange-name">
+          <%= @product.exchange_name %>
+        </div>
+
+        <div class="trade-time">
+        </div>
+      </div>
+    """
+  end
+end
